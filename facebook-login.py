f047dc2119d41a09586c9e7eda5d34e20bcd9469
marcelo mint notebook <marcelocelularxiaomi@gmail.com>
2020-02-11 16:31:51 -0300

    using full parh

--- facebook-login.py
+++ facebook-login.py
@@ -6,4 +6,5 @@ senha = input("informe sua senha: ")
 browser=tadWeb.open_init("https://www.facebook.com/")
 tadWeb.escrever(browser,"//*[@id='email']",login)
 tadWeb.escrever(browser,"//*[@id='pass']",senha)
-tadWeb.clicar(browser,"//*[@id='u_0_b']")
+tadWeb.clicar(browser,"/html/body/div[1]/div[2]/div/div/div/div/div[2]/form/table/tbody/tr[2]/td[3]/label/input")
+#usar sempre full path
